ab6e8e1d68497002c759c1565ec109c0ce123cd9
Nathan Cunha <nathancunha38@gmail.com>
2024-08-19 17:15:29 -0300

    iniciando o crud

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 from flask import Flask, render_template
+from routes.login import login
+from routes.cadastro import cadastro
+from routes.recuperar import recuperar
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    inputs = [
+        {'id': 'email', 'type': 'email', 'placeholder': 'Digite seu email'},
+        {'id': 'senha', 'type': 'password', 'placeholder': 'Digite sua senha'}
+    ]
+    return render_template('index.html', inputs=inputs)
 
 @app.route("/recuperar")
 def recuperar():
     return render_template('recuperar.html')
 
-@app.route("/cadastro")
-def registro():
-    return render_template('cadastro.html')
+@app.route('/cadastro')
+def cadastro():
+    inputs = [
+        {'id': 'nome', 'type': 'text', 'placeholder': 'Digite seu nome'},
+        {'id': 'email', 'type': 'email', 'placeholder': 'Digite seu email'},
+        {'id': 'senha', 'type': 'password', 'placeholder': 'Digite sua senha'},
+        {'id': 'confirmaSenha', 'type': 'password', 'placeholder': 'Confirme sua senha'}
+    ]
+    return render_template('cadastro.html', inputs=inputs)
 
 app.run(debug=True)
---
+++ routes/cadastro.py
@@ -0,0 +1,4 @@
+from flask import Blueprint, render_template
+
+cadastro = Blueprint('cadastro', __name__)
+
