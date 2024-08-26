ad3e167e863e8b80ce93da7208c33d8ce6a943d3
matheus489 <matheus.rubem@ufrpe.br>
2024-08-26 15:01:46 -0300

    criação da pagina perfil, implementação das atualizações de dados da pagina

--- firebaseAuth.py
+++ firebaseAuth.py
@@ -32,6 +32,15 @@ def loginfb(email, password):
         if email_verified == False:
             auth.current_user = None
             return email_verified
+
+def atualiza_perfilfb(nome, email, senha):
+    user = auth.current_user
+    data={'nome':nome,
+          'email':email}
+    db.child('usuarios').child(remove_pontos(email)).update(data)
+    if senha:
+        auth.update_user(user['idToken'], {'password': senha})
+      
         
 def recoverPassword(email):
     auth.send_password_reset_email(email)
--- routes/perfil.py
+++ routes/perfil.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
-
+from flask import Blueprint, render_template, request, redirect,url_for, flash
+import json
+from firebaseAuth import  atualiza_perfilfb
 perfil_routes = Blueprint('perfil', __name__)
 
 """ Rotas de perfil
@@ -12,4 +13,23 @@ perfil_routes = Blueprint('perfil', __name__)
 @perfil_routes.route('/')
 def pagina_perfil():
     """ Retorna a página de perfil """
-    return render_template('perfil.html')
+    
+    def inputs_perfil():
+        imputs = [
+            {'id': 'nome', 'type': 'text', 'placeholder': 'Nome', 'name': 'nome'},
+            {'id': 'email', 'type': 'email', 'placeholder': 'Email', 'name': 'email'},
+            {'id': 'senha', 'type': 'password', 'placeholder': 'Senha', 'name': 'senha'}
+        ]
+        if request.method == 'POST':
+            data = request.form
+        try:
+            atualiza_perfilfb(data['nome'], data['email'], data['senha'])  # tem que fazer essa função
+            flash('Perfil atualizado com sucesso!', 'success')
+            return redirect(url_for('perfil.perfil'))
+        except Exception as e:
+            error_message = json.loads(e.args[1])['error']['message']
+            flash(error_message, 'danger')
+            return render_template('perfil.html', inputs=inputs_perfil())
+
+    return render_template('perfil.html', inputs=inputs_perfil())
+    
