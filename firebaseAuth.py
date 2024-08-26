b4f8fd631b1fd5bb806e6300bd2bfae5c2f3e647
Nathan Cunha <nathancunha38@gmail.com>
2024-08-26 00:04:44 -0300

    Revert "recuperar"
    This reverts commit 483e69a15c38a61b2884f65377b7f6073f2f128c.

--- firebaseAuth.py
+++ firebaseAuth.py
@@ -19,11 +19,17 @@ def cadastrofb(nome, email, password):
         auth.send_email_verification(user['idToken'])
         
 def loginfb(email, password):
-        user = auth.sign_in_with_email_and_password(email, password)
-        user_info = auth.get_account_info(user['idToken'])
+    try:
+        login = auth.sign_in_with_email_and_password(email, password)
+    except:
+        # PopUp dnv dizendo que o usuario não esta cadastrado (tratar dps com if pra ver se é a senha q ta errada)
+        # isso aqui n funciona:
+        print('sla')
+        
+def recoverPassword(email):
+    try:
+        auth.send_password_reset_email(email)
+        print("email enviado")
+    except:
+        print("Email Não Cadastrado")
 
-        # Verifica se o e-mail está verificado
-        email_verified = user_info['users'][0]['emailVerified']
-        if email_verified == False:
-            auth.current_user = None
-            return email_verified
