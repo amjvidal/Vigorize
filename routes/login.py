b1faddaa620ba568ea34775289b4f0f18181b16f
Nathan Cunha <nathancunha38@gmail.com>
2024-08-25 22:05:45 -0300

    Tentativa de implementar o redirecionamento de página

--- routes/login.py
+++ routes/login.py
@@ -29,4 +29,4 @@ def loga():
     except:
         return {'message': 'Falha no login!'}, 400
 
-    return {'message': 'Login realizado com sucesso!'}, 200
+    return redirect(url_for('perfil.pagina_perfil')), 200
