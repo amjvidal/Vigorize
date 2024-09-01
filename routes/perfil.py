78df474201e1d7153749f69884ae25d396c1a0dc
Nathan Cunha <nathancunha38@gmail.com>
2024-09-01 14:34:06 -0300

    Revert "Merge branch 'Mathus-Rubem' of https://github.com/amjvidal/Vigorize into Nathan-Cunha"
    This reverts commit 15cd5d3e27a4ebeb3c3b781d0abc42c27260c474, reversing
    changes made to 10753758bbade4774f1b465eabf96566ddec81ad.

--- routes/perfil.py
+++ routes/perfil.py
@@ -1,46 +1,46 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for, session
 from firebaseAuth import recoverPassword, db, auth, emailDb
 import json
-from calculadora import calculaTMB, calculaPercentGorduraMASC, calculaPercentGorduraFem, calculaIMC
-
 
 perfil_routes = Blueprint('perfil', __name__)
 
+""" Rotas de perfil
+    - /perfil - Get - Retorna a página de perfil
+    - /perfil/editar - PUT - Retorna a página de edição de perfil
+    - /perfil/editar - POST - Edita o perfil do usuário
+    - /perfil/excluir - DELETE - Exclui o perfil do usuário
+"""
+
 @perfil_routes.route('/', methods=['GET','POST'])
 def pagina_perfil():
-    user = auth.current_user
+
+    user=auth.current_user
 
     if user is None:
         flash('Você precisa estar logado para acessar esta página.', 'danger')
         return redirect(url_for('login.pagina_login'))
     
     try:
+
         user_email = emailDb(user['email'])
 
-        # Recupera os dados salvos anteriormente
+
         altura_user = db.child("usuarios").child(user_email).child("altura").get().val()
         peso_user = db.child("usuarios").child(user_email).child("peso").get().val()
         cintura_user = db.child("usuarios").child(user_email).child("cintura").get().val()
         pescoco_user = db.child("usuarios").child(user_email).child("pescoco").get().val()
         sexo_user = db.child("usuarios").child(user_email).child("sexo").get().val()
-        atividade_user = db.child("usuarios").child(user_email).child("fisico").get().val()
-        idade_user = db.child("usuarios").child(user_email).child("dataNas").get().val()
-        
+
 
         inputs = [
-            {'id': 'altura', 'type': 'number', 'value': altura_user, 'name': 'altura', 'label': 'Altura(cm)', 'max': '250', 'min': '100'},
-            {'id': 'peso', 'type': 'number', 'value': peso_user, 'name': 'peso', 'label': 'Peso(kg)', 'max': '500', 'min': '30'},
-            {'id': 'cintura', 'type': 'number', 'value': cintura_user, 'name': 'cintura', 'label': 'Cintura(cm)', 'max': '180', 'min': '30'},
-            {'id': 'pescoco', 'type': 'number', 'value': pescoco_user, 'name': 'pescoco', 'label': 'Pescoço(cm)', 'max': '60', 'min': '20'},
-            {'id': 'sexo', 'type': 'text', 'value': sexo_user, 'name': 'sexo', 'label': 'Sexo', 'disabled': 'true'},
-            {'id': 'fisico', 'type': 'text', 'value': atividade_user, 'name': 'fisico', 'label': 'Atividade Física', 'disabled': 'true'},
-            
-            
+            {'id': 'altura', 'type': 'number', 'value': altura_user,'name': 'altura', 'label': 'Altura(cm)','max': '250','min': '100'},
+            {'id': 'peso', 'type': 'number', 'value': peso_user,'name': 'peso', 'label': 'Peso(kg)','max': '500','min': '30'},
+            {'id': 'cintura', 'type': 'number', 'value': cintura_user,'name': 'cintura', 'label': 'Cintura(cm)','max': '180','min': '30'},
+            {'id': 'pescoco', 'type': 'number', 'value': pescoco_user,'name': 'pescoco', 'label': 'Pescoço(cm)','max': '60','min': '20'},
         ]
-        
         if sexo_user == 'Feminino':
             quadril_user = db.child("usuarios").child(user_email).child("quadril").get().val()
-            inputs.append({'id': 'cintura', 'type': 'number', 'value': quadril_user, 'name': 'cintura', 'label': 'Quadril(cm)', 'max': '180', 'min': '30'})
+            inputs.append({'id': 'cintura', 'type': 'number', 'value': quadril_user,'name': 'cintura', 'label': 'Quadril(cm)', 'max': '180','min': '30'})
 
         if request.method == 'POST':
             action = request.form.get('action')
@@ -59,7 +59,6 @@ def pagina_perfil():
                     if sexo_user == 'Feminino':
                         if data['quadril'] == '':
                             data['quadril'] = quadril_user
-                    
                     
                     db.child("usuarios").child(user_email).update(
                         {'altura': data['altura'],
@@ -80,6 +79,10 @@ def pagina_perfil():
                     flash('Email de recuperação de senha enviado!', 'success')
                 except Exception as e:
                     print(e)
+                    # Captura a exceção e imprime a mensagem de erro
+                    # error_message = json.loads(e.args[1])['error']['message']
+                    # flash(error_message, 'danger')
+                    # return render_template('perfil.html', inputs=inputs)
             
             elif action == 'delete_account':
                 try:
@@ -91,17 +94,9 @@ def pagina_perfil():
                     error_message = str(e)
                     flash(error_message, 'danger')
                     return redirect(url_for('perfil.pagina_perfil'))
-    
+                
     except Exception as e:
         flash('Erro ao acessar o perfil.', 'danger')
         return redirect(url_for('login.pagina_login'))
-    
-    
-    #cal_tmb = calculaTMB(int(peso_user),int(altura_user) ,int(idade_user), sexo_user, atividade_user)
-    if sexo_user == 'Masculino':
-        percent_gordura = calculaPercentGorduraMASC(int(altura_user), int(cintura_user), int(pescoco_user))
-    else:
-        percent_gordura = calculaPercentGorduraFem(int(altura_user), int(cintura_user), int(pescoco_user), int(quadril_user))
-    imc = calculaIMC(int(peso_user), int(altura_user))
             
-    return render_template('perfil.html', inputs=inputs, percent_gordura=percent_gordura, imc=imc)
+    return render_template('perfil.html', inputs=inputs)
