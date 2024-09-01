c9e069924ad2e07de990bed816a0caabe0509623
Luis Davi <d7a8v9i@gmail.com>
2024-09-01 03:06:24 -0300

    dados filtrados

--- routes/tabelaNutricional.py
+++ routes/tabelaNutricional.py
@@ -1,43 +1,29 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
+import json
 
+tabelaNutricao = Blueprint('tabelaNutricional', __name__)
 
+with open("dados.json",encoding="utf-8") as alimentos:
+    dados=json.load(alimentos)
 
-#falta trazer o json TACO
-alimentos=[]
+inflated_buttons = []
 
-tabelaNutricao = Blueprint('tabelaNutri', __name__)
-
-@tabelaNutricao.route('/', methods=['GET'])
+@tabelaNutricao.route('/', methods=['GET','POST'])
 def tabelaNutri():  
-    input=[
+    input=[{'id':'barraPesquisa', 'type':'text', 'placeholder':'Busque seu alimento: ','layout':'inline' }]
+    if request.method=='POST':
+        selected_item = request.form.get('item')
+        if selected_item:
+            inflated_buttons.append(selected_item)
 
-    ]
-
-    return render_template('tabelaNutricional.html')
+    return render_template('tabelaNutricional.html',items=dados,buttons=inflated_buttons)
 
 
 # umidade (%) - Energia (kcal) - proteina (g) gorduraTotal(g) - carboidratos (g) - FibraAlimentar(g) medidas por 100g
 
 
-
-def gordurasTotal(caloria,proteina,carboidratos):
-    gordurasTotal = (caloria-(proteina*4)-(carboidratos*4))/9
-    return round(gordurasTotal,2)
-
-def getClasse(nome):
-    
-    return 
-
-    
-
-def excluiCru(nome):
-    if not'Salmão' in nome:
-        if 'cru' in nome:
-            return None
-        else:
-            return nome
-    else:
-        return nome
-
-
-
+def getCategory(description):
+    for item in dados:
+        if item["description"] == description:
+            return item["category"]
+    return None  
