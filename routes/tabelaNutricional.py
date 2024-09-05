d8c54b1a6ebca58b772b62599772221928e07af3
Luis Davi <d7a8v9i@gmail.com>
2024-09-05 19:07:22 -0300

    mostrando apenas os nomes na barra de pesquisa, e consertando o bug de atualizar a pag

--- routes/tabelaNutricional.py
+++ routes/tabelaNutricional.py
@@ -1,29 +1,23 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Blueprint, render_template, request, flash, redirect, url_for, session
 import json
 
 tabelaNutricao = Blueprint('tabelaNutricional', __name__)
 
-with open("dados.json",encoding="utf-8") as alimentos:
-    dados=json.load(alimentos)
+with open("dados.json", encoding="utf-8") as alimentos:
+    dados = json.load(alimentos)
 
-inflated_buttons = []
-
-@tabelaNutricao.route('/', methods=['GET','POST'])
-def tabelaNutri():  
-    input=[{'id':'barraPesquisa', 'type':'text', 'placeholder':'Busque seu alimento: ','layout':'inline' }]
-    if request.method=='POST':
+@tabelaNutricao.route('/', methods=['GET', 'POST'])
+def tabelaNutri():
+    
+    if 'inflated_buttons' not in session:
+        session['inflated_buttons'] = []
+    
+    if request.method == 'POST':
         selected_item = request.form.get('item')
-        if selected_item:
-            inflated_buttons.append(selected_item)
-
-    return render_template('tabelaNutricional.html',items=dados,buttons=inflated_buttons)
-
-
-# umidade (%) - Energia (kcal) - proteina (g) gorduraTotal(g) - carboidratos (g) - FibraAlimentar(g) medidas por 100g
-
-
-def getCategory(description):
-    for item in dados:
-        if item["description"] == description:
-            return item["category"]
-    return None  
+        if selected_item and selected_item not in session['inflated_buttons']:
+           
+            session['inflated_buttons'].append(selected_item)
+            session.modified = True  
+            
+    
+    return render_template('tabelaNutricional.html', items=dados, buttons=session['inflated_buttons'])
