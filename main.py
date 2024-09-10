5315d990c44a2b5a70d95ed02bb5ba74a9c160ef
matheus489 <matheus.rubem@ufrpe.br>
2024-09-10 12:51:58 -0300

    Refactor main.py and main.css files

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from flask import Flask
-from flask_cors import CORS
+# from flask_cors import CORS
 from firebaseAuth import profilePics_folder
 app = Flask(__name__)
-CORS(app)
+# CORS(app)
 app.secret_key = 'vigorizee'
 
 from routes.home import home_routes
