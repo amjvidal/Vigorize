a81b9c68712a1a8fe111cf1898e28c43e8626da5
Luis Davi <d7a8v9i@gmail.com>
2024-09-09 22:13:39 -0300

    função Delete da Tabela de nutrição funcionando

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_cors import CORS
 from firebaseAuth import profilePics_folder
 app = Flask(__name__)
+CORS(app)
 app.secret_key = 'vigorizee'
 
 from routes.home import home_routes
